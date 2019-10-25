7a2a34b209eeac131bb5d403c2ee82cca6cde00b
Dement0 <ilkeer.ozgur@gmail.com>
2019-10-25 17:19:13 +0200

    Add Website Button

--- app/src/main/java/com/example/trackmoney/MainActivity.kt
+++ app/src/main/java/com/example/trackmoney/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.trackmoney
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Add onClickListener for visit website button
+        visit_website_button.setOnClickListener { visitWebsite() }
     }
 
     // Called when user taps the Add Income/Expense button
     fun addIncomeExpense(view: View) {
         val intent = Intent(this, AddIncomeExpenseActivity::class.java)
         startActivity(intent)
+    }
+
+    // Called when user taps "Visit Website" button
+    fun visitWebsite() {
+        val webIntent: Intent = Uri.parse("https://www.codingavision.com").let {
+                webpage -> Intent(Intent.ACTION_VIEW, webpage)
+        }
+        startActivity(webIntent)
     }
 }
