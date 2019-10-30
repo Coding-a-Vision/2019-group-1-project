11a67800062cc03fbc13dd17435294bbf367ae2d
giovanni <giovanni.vitiello10@gmail.com>
2019-10-30 10:44:46 +0100

    added bottom navigation, shifted activity main.xml into home fragment.xml. Improved layout dedign app

--- app/src/main/java/com/example/trackmoney/MainActivity.kt
+++ app/src/main/java/com/example/trackmoney/MainActivity.kt
@@ -4,6 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +18,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        //By calling this method, the title in the action bar will automatically be updated when
+        // the destination changes
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+    
     // Called when user taps the Add Income/Expense button
     fun addIncomeExpense(view: View) {
         val intent = Intent(this, AddIncomeExpenseActivity::class.java)
---
+++ app/src/main/java/com/example/trackmoney/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.trackmoney.ui.dashboard
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = ""
+    }
+    val text: LiveData<String> = _text
+}
