5672d41a2447419811573e82809c171e11727fb5
giovanni <giovanni.vitiello10@gmail.com>
2019-11-08 09:54:52 +0100

    add navigation for fabButton, and changed the income_expense_activity with a income_expense_activity_fragment

--- app/src/main/java/com/example/trackmoney/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trackmoney/ui/home/HomeFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.trackmoney.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
@@ -25,5 +28,15 @@ class HomeFragment : Fragment() {
             textView.text = it
         })*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Called when user taps the Add Income/Expense button
+        val fabButton = view.findViewById<FloatingActionButton>(R.id.fab)
+        fabButton.setOnClickListener {
+            findNavController().navigate(R.id.addIncomeExpense)
+        }
     }
 }
