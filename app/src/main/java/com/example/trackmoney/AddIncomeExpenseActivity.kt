b3697c8afb88e3e1f169f30be74982dbdd627a23
Dement0 <ilkeer.ozgur@gmail.com>
2019-11-13 17:07:18 +0100

    Add DatePicker

--- app/src/main/java/com/example/trackmoney/AddIncomeExpenseActivity.kt
+++ app/src/main/java/com/example/trackmoney/AddIncomeExpenseActivity.kt
@@ -1,20 +1,53 @@
 package com.example.trackmoney
 
+import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.trackmoney.ui.home.HomeFragment
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddIncomeExpenseActivity : AppCompatActivity() {
+
+    lateinit var date_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_income_expense)
+
+        date_button = findViewById(R.id.income_expense_date)
     }
 
     // Called when user taps the Add Income/Expense button
     fun turnToMain(view: View) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+
+        val intent = Intent()
+        val result_text = findViewById<EditText>(R.id.income_expense_amount).text.toString()
+
+        intent.putExtra("ADD_IE_RESULT",result_text)
+
+        if(result_text.isEmpty()) {
+            setResult((Activity.RESULT_CANCELED))
+        } else {
+            setResult(Activity.RESULT_OK, intent)
+        }
+
+        finish()
+    }
+
+    fun showDatePickerDialog(v: View) {
+        val newFragment = DatePickerFragment()
+        newFragment.show(supportFragmentManager, "datePicker")
+    }
+
+    fun updateDate(updated_date: String) {
+        this.date_button.text = updated_date
     }
 }
--- app/src/main/java/com/example/trackmoney/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trackmoney/ui/home/HomeFragment.kt
@@ -1,17 +1,21 @@
 package com.example.trackmoney.ui.home
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.trackmoney.AddIncomeExpenseActivity
+import com.example.trackmoney.MainActivity
 import com.example.trackmoney.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -25,25 +29,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        // Initialize the ViewModel
+        // Get the ViewModel
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
 
-        // Find the root
+        // Inflate the layout to the fragment
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-
-        // Get a reference for the textView in home fragment
-        val textView: TextView = root.findViewById(R.id.text_home)
-
-        homeViewModel.addIncomeExpenseData("fourth")
-
-        // Change textView if HomeViewModel text changes
-        homeViewModel.text.observe(this, Observer {
-            var dummyText: String = ""
-            for(dummy_data in it) {
-                dummyText += dummy_data + "\n"
-            }
-            textView.text = dummyText
-        })
 
         return root
     }
@@ -51,11 +41,48 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        updateHomeText()
+
         // Called when user taps the Add Income/Expense button
         val fabButton = view.findViewById<FloatingActionButton>(R.id.fab)
         fabButton.setOnClickListener {
             val intent = Intent(activity, AddIncomeExpenseActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, 1000)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1000 && resultCode == Activity.RESULT_OK) {
+            if(data != null) {
+                Log.i("ACTIVITY RESULT", data?.extras?.get("ADD_IE_RESULT").toString())
+                val data_callback = data?.extras?.get("ADD_IE_RESULT").toString()
+                homeViewModel.addIncomeExpenseData(data_callback)
+                Toast.makeText(context, "Data added.", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.i("ACTIVITY RESULT", "was NULL.")
+                Toast.makeText(context, "Error, please try again.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Log.i("ACTIVITY RESULT", "was NULL.")
+            Toast.makeText(context, "Error, please try again.", Toast.LENGTH_SHORT).show()
+        }
+
+        updateHomeText()
+    }
+
+    private fun updateHomeText() {
+        // Get a reference for the textView to change in home fragment
+        val home_text_view: TextView = view!!.findViewById(R.id.text_home)
+
+        // Set home_text_view content getting the data from the ViewModel
+        homeViewModel.text.observe(this, Observer {
+            var dummyText: String = ""
+            for(dummy_data in it) {
+                dummyText += dummy_data + "\n"
+            }
+            home_text_view.text = dummyText
+        })
     }
 }
